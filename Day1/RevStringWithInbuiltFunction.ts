8f210bf2b0509dba2838d05bd4e493d88b9dfccc
anirudho747 <anirudho.sengupta@live.com>
2025-01-02 08:43:00 +0530

    Test Lead

--- Day1/RevStringWithInbuiltFunction.ts
+++ Day1/RevStringWithInbuiltFunction.ts
@@ -1,6 +1,10 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-let str :string = prompt('Enter the String which needs to be reversed ');
+let str = prompt('Enter the String which needs to be reversed ');
 
 console.log(str);
+
+let strArray  = str.split('');
+
+console.log(strArray.reverse().join(''));
