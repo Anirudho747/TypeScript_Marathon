2fb3cb13c5ffa1f860ea59385b714c3f9b4e3fdf
anirudho747 <anirudho.sengupta@live.com>
2025-01-02 09:15:29 +0530

    TTL : Corrections done (Self and Suggested)

--- Day1/RevStringWithoutInBuiltFunction.ts
+++ Day1/RevStringWithoutInBuiltFunction.ts
@@ -1,10 +1,12 @@
 import promptSync from 'prompt-sync';
 
 const prompt = promptSync();
-let str = prompt("Enter String").toLowerCase();
-
-for(let i = str.length-1;i>=0;i--)
-{
-    process.stdout.write(str[i]);
+let str = prompt("Enter String");
+if(str.length < 2){
+    console.log("Please enter Strings lomger than 1 character")
 }
-
+else {
+    for (let i = str.length - 1; i >= 0; i--) {
+        process.stdout.write(str[i]);
+    }
+}
