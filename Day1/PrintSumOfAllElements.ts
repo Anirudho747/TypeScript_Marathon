4c9f3a3049d30066a313fc3780aafa2e7f44ba90
anirudho747 <anirudho.sengupta@live.com>
2025-01-02 09:22:16 +0530

    TTL : Sum Of all Array Elements with and without Inbuilt functions

---
+++ Day1/PrintSumOfAllElements.ts
@@ -0,0 +1,9 @@
+let a = [10,20,30,40,50,60,70,80,90,100];
+
+let sum =0;
+
+for(let i = 0; i < a.length; i++) {
+    sum += a[i];
+}
+
+console.log(sum);
---
+++ Day1/PrintSumOfAllElementsUsingInbuiltFunction.ts
@@ -0,0 +1,7 @@
+let a = [10,20,30,40,50,60,70,80,90,100];
+
+let sum =0;
+
+sum = a.reduce((acc,num)=>acc+num);
+
+console.log(sum);
